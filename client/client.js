e0928bd235069f5927d8f87dd684d33875ca4419
Kinga Kozioł <kinga.koziol6@gmail.com>
2017-03-06 14:31:55 +0100

    Hook App.

--- client/client.js
+++ client/client.js
@@ -1,1 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../components/App.js';
 
+render(<App/>, document.getElementById('app'));
+//define the encompassing component, choose dom element we want to mount it to
--- components/App.js
+++ components/App.js
@@ -1,1 +1,11 @@
+import React, { Component } from 'react';
 
+class App extends Component {
+
+  render() {
+    return <div>This is text.</div>
+  }
+
+}
+
+export default App;
